31a0df0154d9cc6387af9f347bca4fe61644efc9
samar-almas <samaralmas8@gmail.com>
2023-05-28 23:29:30 +0500

    Change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import CommingSoon from "./pages/CommingSoon";
-
 function App() {
-  return <CommingSoon />;
+  return <h1 className="text-center text-4xl font-bold mt-4">Hello</h1>;
 }
 
 export default App;
